25e62aaa8c27bb5843ceac09cb97f367b2123745
Min-ho Kim <xho1995@gmail.com>
2020-12-19 23:20:03 +0900

    initialize the project

---
+++ SwiftUI Tutorials/Creating and Combining Views/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Kim, Min Ho on 2020/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
