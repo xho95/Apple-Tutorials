afd8daee3dac8d46729b5fef005c713001442e5d
Min-ho Kim <xho1995@gmail.com>
2020-12-22 21:07:03 +0900

    initializes the project for the Handling User Input

---
+++ SwiftUI Tutorials/03. Handling User Input/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kim, Min Ho on 2020/12/19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI Tutorials/03. Handling User Input/Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Kim, Min Ho on 2020/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
