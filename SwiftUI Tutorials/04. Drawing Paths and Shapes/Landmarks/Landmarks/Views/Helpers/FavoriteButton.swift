22a718c20747b3b5011ca2ed4a2a8e1027ea9463
Min-ho Kim <xho1995@gmail.com>
2020-12-23 20:40:48 +0900

    copies the Landmarks from the previous tutorial

---
+++ SwiftUI Tutorials/04. Drawing Paths and Shapes/Landmarks/Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Kim, Min Ho on 2020/12/22.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
