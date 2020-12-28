eb84f52567bff48fd3b37fb5151c5ad328eacb80
Min-ho Kim <xho1995@gmail.com>
2020-12-28 21:47:23 +0900

    initializes the tutorial for Interfacing with UIKit

---
+++ SwiftUI Tutorials/08. Interfacing with UIKit/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kim, Min Ho on 2020/12/19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
---
+++ SwiftUI Tutorials/08. Interfacing with UIKit/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,52 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Kim, Min Ho on 2020/12/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .listStyle(InsetListStyle())
+            .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Image(systemName: "person.crop.circle")
+                        .accessibilityLabel("User Profile")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
