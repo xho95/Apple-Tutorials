682fae8c7efff16e2f8c04447b4aa56608709eaa
Min-ho Kim <xho1995@gmail.com>
2021-01-19 21:44:42 +0900

    finishes the Creating a CardView part

---
+++ iOS App Dev with SwiftUI Tutorials/02. Views/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Kim, Min Ho on 2021/01/17.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
