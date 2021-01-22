032bb42a4af8f27bd37abbd10ef6d67988e35418
Min-ho Kim <xho1995@gmail.com>
2021-01-22 23:13:12 +0900

    initializes the tutorial for Navigation and Modal Presentation

---
+++ iOS App Dev with SwiftUI Tutorials/03. Navigation and Modal Presentation/Scrumdinger/Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,28 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Kim, Min Ho on 2021/01/21.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    //.background(scrum.color)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
